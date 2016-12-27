85920b158082e5656204230562993d97ceae3162
Hans Wannop <hanswannop@me.com>
2016-12-27 22:48:07 +0900

    Load rom

--- src/cry_boy.cr
+++ src/cry_boy.cr
@@ -2,12 +2,14 @@ require "./rom"
 require "./cpu"
 
 class CryBoy
+
   def initialize(rom_path : String)
     @running = true
-    @rom = ROM.new rom_path
+    @rom = ROM.new(rom_path)
     @cpu = CPU.new
   end
 
   def step
   end
+
 end
--- src/rom.cr
+++ src/rom.cr
@@ -1,4 +1,22 @@
 class ROM
-  def initialize(rom_path)
+  @data : Slice(UInt8)
+  @name : String
+
+  NAME_OFFSET = 0x134
+
+  def initialize(path : String)
+    @data = load_rom(path)
+    @name = String.new(@data[NAME_OFFSET,16])
+    puts @data
+    puts @name
+  end
+
+  def load_rom(path : String)
+    abort("Could not load ROM from #{path}") unless File.file?(path) 
+    File.open(path) do |file|
+      data = Slice(UInt8).new file.size.to_i
+      file.read data
+      return data
+    end
   end
 end
